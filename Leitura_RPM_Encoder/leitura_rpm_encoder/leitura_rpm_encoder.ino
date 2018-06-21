f2fb1292e31ab7a95f702bd4672547a30180be72
odair-silva <odair_silva@outlook.com>
2018-06-21 17:47:55 -0300

    pinos funcionando, due lendo RPM até 6xxx RPM

--- Leitura_RPM_Encoder/leitura_rpm_encoder/leitura_rpm_encoder.ino
+++ Leitura_RPM_Encoder/leitura_rpm_encoder/leitura_rpm_encoder.ino
@@ -2,13 +2,14 @@
 //Autor: Arduino e Cia
 
 //Pino ligado ao pino D0 do sensor
-int pino_D0 = 2;
+int pino_D0 = 32;
 int rpm;
 volatile byte pulsos;
 unsigned long timeold;
+int cont;
 
 //Altere o numero abaixo de acordo com o seu disco encoder
-unsigned int pulsos_por_volta = 10;
+unsigned int pulsos_por_volta = 20;
 
 void contador()
 {
@@ -23,10 +24,11 @@ void setup()
   pinMode(pino_D0, INPUT);
   //Interrupcao 0 - pino digital 2
   //Aciona o contador a cada pulso
-  attachInterrupt(0, contador, FALLING);
+  attachInterrupt(digitalPinToInterrupt(32), contador, FALLING);
   pulsos = 0;
   rpm = 0;
   timeold = 0;
+  cont = 0;
 }
 
 void loop()
@@ -34,16 +36,18 @@ void loop()
   //Atualiza contador a cada segundo
   if (millis() - timeold >= 100)
   {
+    cont = pulsos;
     //Desabilita interrupcao durante o calculo
-    detachInterrupt(0);
+    detachInterrupt(digitalPinToInterrupt(32));
     rpm = (60 * 1000 / pulsos_por_volta ) / (millis() - timeold) * pulsos;
     timeold = millis();
     pulsos = 0;
+    cont++;
 
     //Mostra o valor de RPM no serial monitor
     Serial.print("RPM = ");
     Serial.println(rpm, DEC);
     //Habilita interrupcao
-    attachInterrupt(0, contador, FALLING);
+    attachInterrupt(digitalPinToInterrupt(32), contador, FALLING);
   }
 }
